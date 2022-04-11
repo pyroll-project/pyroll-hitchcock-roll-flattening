5c44f19bddac3e9d28d033e25adf8be01d27c029
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2022-04-11 10:34:47 +0200

    Added specs

--- pyroll_hitchcook_roll_flattening/specs.py
+++ pyroll_hitchcook_roll_flattening/specs.py
@@ -11,9 +11,16 @@ def roll_youngs_modulus(roll_pass):
     """Youngs modulus of the roll material."""
 
 
+@RollPass.hookimpl
+def previous_roll_force(roll_pass):
+    """Roll force of previous iteration step"""
+
+
+@RollPass.hookimpl
+def previous_nominal_roll_radius(roll_pass):
+    """Nominal roll radius of previous iteration step"""
+
+
 @RollPass.hookspec
 def nominal_radius_flattened_radius_ratio(roll_pass):
     """Calculate the ratio between nominal radius and flattened radius of roll."""
-
-
-
