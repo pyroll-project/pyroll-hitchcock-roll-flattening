3a32f02e0bc0028f995513a135cc5d143bb4978e
Max Weiner <max.weiner@imf.tu-freiberg.de>
2022-12-21 11:23:59 +0100

    upd to 2.0a

--- pyroll/hitchcock_roll_flattening/__init__.py
+++ pyroll/hitchcock_roll_flattening/__init__.py
@@ -1,14 +1,83 @@
 from pyroll.core import RollPass, Roll
-from pyroll.ui import Reporter
 
-from . import specs
-from . import impls
-from . import report
+import numpy as np
+import logging
+from pyroll.core import RollPass, Roll, Hook
 
-Roll.plugin_manager.add_hookspecs(specs)
-RollPass.Roll.plugin_manager.add_hookspecs(specs)
+log = logging.getLogger(__name__)
 
-Roll.plugin_manager.register(impls)
-RollPass.Roll.plugin_manager.register(impls)
+RollPass.Roll.flattening_ratio = Hook[float]()
+"""The ratio between flattened and nominal radius of the roll."""
 
-Reporter.plugin_manager.register(report)
+RollPass.Roll.flattened_radius = Hook[float]()
+"""Flattened roll radius acc. to Hitchcock."""
+
+
+@Roll.poissons_ratio
+def default_poissons_ratio(roll: Roll):
+    """Default implementation for steel rolls."""
+    return 0.3
+
+
+@Roll.elastic_modulus
+def default_elastic_modulus(roll: Roll):
+    """Default implementation for steel rolls."""
+    return 210e9
+
+
+@RollPass.Roll.flattening_ratio
+def flattening_ratio(self: RollPass.Roll):
+    """Calculates the ratio between flattened and initial roll radius using Hitchcooks formula."""
+    roll_pass = self.roll_pass()
+
+    elastic_constant = (16 * (1 - self.poissons_ratio ** 2)) / (np.pi * self.elastic_modulus)
+    height_change = roll_pass.in_profile.equivalent_rectangle.height - roll_pass.out_profile.equivalent_rectangle.height
+    mean_width = (
+                         roll_pass.in_profile.equivalent_rectangle.width + roll_pass.out_profile.equivalent_rectangle.width) / 2
+
+    flattening_hitchcock = (elastic_constant * roll_pass.roll_force) / (height_change * mean_width)
+
+    if flattening_hitchcock < 4.235:
+        ratio = 1 + flattening_hitchcock
+
+    else:
+        ratio = 2 * flattening_hitchcock ** (2 / 3)
+
+    log.info(f"Calculated radius ratio of {ratio:.2f}")
+    return ratio
+
+
+@RollPass.Roll.flattened_radius
+def flattened_radius(self: RollPass.Roll):
+    """Calculates the flattened radius."""
+    roll_pass = self.roll_pass()
+
+    if "roll_force" not in roll_pass.__dict__:
+        radius = self.nominal_radius
+    else:
+
+        radius = roll_pass.roll.flattening_ratio * roll_pass.roll.nominal_radius
+        log.info(f"Calculated a roll radius of {radius * 1e3:.2f} mm using Hitchcook's model!")
+
+    return radius
+
+
+@RollPass.Roll.max_radius
+def max_radius(self: RollPass.Roll):
+    if self.has_value("flattened_radius"):
+        radius = self.flattened_radius
+        return radius
+
+
+@RollPass.Roll.min_radius
+def min_radius(self: RollPass.Roll):
+    if self.has_value("flattened_radius"):
+        radius = self.flattened_radius - self.groove.depth
+        return radius
+
+
+@RollPass.Roll.working_radius
+def working_radius(self: RollPass.Roll):
+    if self.has_value("flattened_radius"):
+        radius = self.flattened_radius - self.groove.cross_section.centroid.y
+        return radius
--- tests/test_solve.py
+++ tests/test_solve.py
@@ -1,25 +1,58 @@
 import logging
 from pathlib import Path
-
-from pyroll.core import solve
-from pyroll.ui import Reporter
+from pyroll.core import Profile, Roll, RollPass, Transport, RoundGroove, CircularOvalGroove, PassSequence
 
 
-def test_solve(tmp_path: Path, caplog):
-    caplog.set_level(logging.DEBUG, "pyroll")
+def test_solve_min(tmp_path: Path, caplog):
+    caplog.set_level(logging.DEBUG, logger="pyroll")
 
-    from pyroll import hitchcock_roll_flattening
+    import pyroll.hitchcock_roll_flattening
 
-    from pyroll.ui.cli.res import input_trio
+    in_profile = Profile.round(
+        diameter=30e-3,
+        temperature=1200 + 273.15,
+        strain=0,
+        material=["C45", "steel"],
+        flow_stress=100e6
+    )
 
-    solve(input_trio.sequence, input_trio.in_profile)
+    sequence = PassSequence([
+        RollPass(
+            label="Oval I",
+            roll=Roll(
+                groove=CircularOvalGroove(
+                    depth=8e-3,
+                    r1=6e-3,
+                    r2=40e-3
+                ),
+                nominal_radius=160e-3,
+                rotational_frequency=1
+            ),
+            gap=2e-3,
+        ),
+        Transport(
+            label="I => II",
+            duration=1
+        ),
+        RollPass(
+            label="Round II",
+            roll=Roll(
+                groove=RoundGroove(
+                    r1=1e-3,
+                    r2=12.5e-3,
+                    depth=11.5e-3
+                ),
+                nominal_radius=160e-3,
+                rotational_frequency=1
+            ),
+            gap=2e-3,
+        ),
+    ])
 
-    report = Reporter().render(input_trio.sequence)
+    try:
+        sequence.solve(in_profile)
+    finally:
+        print("\nLog:")
+        print(caplog.text)
 
-    report_file = tmp_path / "report.html"
-    report_file.write_text(report)
-    print()
-    print(report_file)
-
-    print()
-    print(caplog.text)
+    assert sequence[0].roll.has_cached("flattened_radius")
